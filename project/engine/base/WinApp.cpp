54956948bdb18df420c1fbb0a94fcb41c43a8787
Shinnosuke612 <K024G0100@g.neec.ac.jp>
2026-09-09 06:49:00 +0900

    chore(editor): adjust particle shortcut and app title

--- project/engine/base/WinApp.cpp
+++ project/engine/base/WinApp.cpp
@@ -55,7 +55,7 @@ void WinApp::Initialize(){
 	// ウィンドウの生成
 	hwnd = CreateWindow(
 		wc.lpszClassName,      // 利用するクラス名
-		L"CG2",                // タイトルバーの文字（何でも良い）
+		L"3044_ツナになりたい",                // タイトルバーの文字（何でも良い）
 		WS_OVERLAPPEDWINDOW,   // よく見るウィンドウスタイル
 		CW_USEDEFAULT,         // 表示X座標（Windowsに任せる）
 		CW_USEDEFAULT,         // 表示Y座標（WindowsOSに任せる）
